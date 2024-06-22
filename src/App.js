1f1adea3b4de2587f4fadd309d8068facedd5542
Yibeltal <yibeltalawoke058@gmail.com>
2024-06-22 13:24:29 +0300

    yes

---
+++ src/App.js
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+import './App.css';
+import MenuBar from './components/MenuBar' 
+function App () {
+  return (
+      <>
+      <MenuBar />
+      </>
+    )}
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+>>>>>>> bb00f6e180b41e3a77a790c9dc6bd9fe8cfb5d53
+export default App;
